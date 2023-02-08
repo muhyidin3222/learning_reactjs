b8aaadcc67f76a83b23cbc201a084efc81000d3c
muhyidin3222 <muhyidin480@gmail.com>
2023-02-08 18:48:37 +0700

    propTypes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
-import React, { Suspense, lazy } from 'react';
-import { Skeleton } from 'antd'
-
-const Component1 = lazy(() => import('./Component1'));
-const Component2 = lazy(() => delayForDemo(import('./Component2')));
-const Component3 = lazy(() => delayForDemo(import('./Component3')));
-
-// Add a fixed delay so you can see the loading state
-function delayForDemo(promise) {
-  return new Promise(resolve => {
-    setTimeout(resolve, 500);
-  }).then(() => promise);
-}
+import React from 'react';
+import Component1 from './Component1'
 
 function App() {
-  return <Suspense fallback={<Skeleton />}>
-    <div style={{ display: 'flex' }}>
-      <Component1 />
-      <Suspense fallback={<Skeleton />}>
-        <Component2 />
-      </Suspense>
-      <Suspense fallback={<Skeleton />}>
-        <Component3 />
-      </Suspense>
-    </div>
-  </Suspense>
-    ;
+  return (
+    <>
+      <Component1
+        email={"tes@gmail.com"}
+        umur={12}
+        nama="cah bagus"
+        aktif={false}
+        children={["hallo", "aman"]}
+        buttonClick={() => { }}
+        teman={{
+          nama: "wahyu"
+        }}
+      />
+    </>
+  );
 }
 
 export default App;
--- src/Component1.js
+++ src/Component1.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import { Card } from 'antd'
+import PropsType from 'prop-types'
 
-function Component1() {
+function Component1({
+    email,
+    umur,
+    nama,
+    aktif,
+    children,
+    buttonClick,
+    teman,
+}) {
+    console.log(teman)
     return <>
         <Card title="Component1" bordered={false} style={{ width: 300, margin: 10 }}>
-            <p>Card content</p>
-            <p>Card content</p>
-            <p>Card content</p>
+            <p>{email}</p>
+            <p>{umur}</p>
+            <p>{nama}</p>
+            <button onClick={buttonClick} >Click</button>
         </Card>
     </>;
+}
+
+Component1.propTypes = {
+    email: PropsType.string,
+    umur: PropsType.oneOf([12, 13]),
+    nama: PropsType.string.isRequired,
+    aktif: PropsType.bool,
+    children: PropsType.arrayOf(PropsType.string),
+    buttonClick: PropsType.func.isRequired,
+    teman: PropsType.objectOf(PropsType.string),
 }
 
 export default Component1;
